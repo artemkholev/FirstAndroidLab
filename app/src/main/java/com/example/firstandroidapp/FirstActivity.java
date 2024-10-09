5e2aa629dd661928300c3d2fa961ae6c195400b2
artem-kholev <artemkholev@yandex.ru>
2024-10-09 23:28:44 +0300

    ADD: event on button in first activity

--- app/src/main/java/com/example/firstandroidapp/FirstActivity.java
+++ app/src/main/java/com/example/firstandroidapp/FirstActivity.java
@@ -1,7 +1,9 @@
 package com.example.firstandroidapp;
 
 import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -17,10 +19,18 @@ public class FirstActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_first);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.first_activity), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        Button startSecondActivityButton = findViewById(R.id.openSecondActivity);
+        startSecondActivityButton.setOnClickListener(view -> {
+            Intent intent = new Intent(FirstActivity.this, SecondActivity.class);
+            intent.putExtra("welcome_phrase", "Hello");
+            startActivity(intent);
         });
     }
 }
